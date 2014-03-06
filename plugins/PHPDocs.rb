065fb2b502b44c345352d8ca59e3ff5e96ffd69b
Brian Buller <brbuller@gmail.com>
2014-03-06 09:19:23 -0600

    Added PHPDocs Plugin

---
+++ plugins/PHPDocs.rb
@@ -0,0 +1,34 @@
+require 'cinch'
+require 'open-uri'
+require 'uri'
+
+class PHPDocs
+  include Cinch::Plugin
+
+  set :prefix, /^!/
+
+  match /php (.+)/, method: :php_doc
+
+  def php_doc(m, func_name)
+    # Lookup PHP Docs
+    file = open('http://php.net/'+URI.escape(func_name))
+    contents = file.read
+    if !contents.include? "methodsynopsis"
+      m.reply "Function not found."
+      return
+    end
+    summary = contents.scan /<p class="refpurpose">(.+?)<\/p>/m
+    m.reply clean_string summary[0][0]
+    result = contents.scan /<div class="methodsynopsis dc-description">(.+?)<\/div>/m
+    m.reply clean_string result[0][0]
+  end
+
+  def clean_string(inc_string)
+    return inc_string
+              .gsub(/<[^>]*>/ui,'')
+              .gsub(/\n/,'')
+              .gsub(/\s\s+/,' ')
+              .gsub('&mdash;','-')
+              .strip
+  end
+end
